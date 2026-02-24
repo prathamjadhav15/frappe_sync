785ebc010cb6bfe5c5c22485d72eee548f4f9e7c
prathameshjadhav <prathamjadhav052@gmail.com>
2026-02-24 12:22:37 +0000

    add alert message

--- frappe_sync/frappe_sync/doctype/sync_connection/sync_connection.js
+++ frappe_sync/frappe_sync/doctype/sync_connection/sync_connection.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on("Sync Connection", {
 	test_connection_button(frm) {
+		frappe.show_alert({ message: __("Testing connection..."), indicator: "blue" });
 		frappe.call({
 			method: "frappe_sync.frappe_sync.doctype.sync_connection.sync_connection.test_connection",
 			args: { doc_name: frm.doc.name },
